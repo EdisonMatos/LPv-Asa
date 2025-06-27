a65b4e9babc2a8b4c57d013311f450e508cacf2c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-27 14:21:37 -0300

    fix: Logo

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -18,15 +18,14 @@ export default function HeroLP() {
       >
         <SectionWrapper className="">
           <div className="flex w-full ">
-            <div className="flex phone1:flex-col-reverse gap-[40px]  desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[46px] desktop1:pt-[68px] desktop1:pb-[96px]">
+            <div className="flex phone1:flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[46px] desktop1:pt-[68px] desktop1:pb-[96px]">
               <div className="flex  flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]">
                 <MotionDivDownToUp className="flex justify-center desktop1:justify-start mb-12">
-                  {/* <img
+                  <img
                     src={contentLp01.hero.logo.logoImg}
                     alt={contentLp01.hero.logo.alt}
-                    className="mb-[60px] max-w-[280px] phone2:max-w-[320px] phone3:max-w-[270px]"
-                  /> */}
-                  <h1 className="text-title3 desktop1:text-title5 text-white">Hingryd Rokar Previdenciarista</h1>
+                    className="mb-[30px] max-w-[240px] phone2:max-w-[290px] phone3:max-w-[270px]"
+                  />
                 </MotionDivDownToUp>
                 {/* <MotionDivDownToUp>
                     <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont phone1:text-paragraph4">
